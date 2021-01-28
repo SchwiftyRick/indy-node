e2d63fa18a81a67e259c304cd29a22a3476026be
Robin Klemens <robin.klemens@deutschebahn.com>
2021-01-28 17:18:51 +0100

    test with new deb files

--- build-scripts/test/create_dir.sh
+++ build-scripts/test/create_dir.sh
@@ -11,7 +11,7 @@ done
 
 # generate base config if not exists
 if [ ! -f /etc/indy/indy_config.py ]; then
-    echo "NETWORK_NAME = 'sandbox'" | tee -a /etc/indy/indy_config.py
+    echo "NETWORK_NAME = 'ARINDYNET'" | tee -a /etc/indy/indy_config.py
 
     echo "LEDGER_DIR = '/var/lib/indy'" | tee -a /etc/indy/indy_config.py
     echo "LOG_DIR = '/var/log/indy'"  | tee -a /etc/indy/indy_config.py
